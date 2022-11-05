60bd53221057dd43111478acfe1fa25bf307fd15
Shubh-Conestoga <spatel8035@conestogac.on.ca>
2022-11-05 18:45:21 -0400

    Home section done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Header from "./component/Header";
+import About from "./component/section/About";
+import Home from "./component/section/Home";
 
 function App() {
+  useEffect(() => {
+    console.log(window.myCustomObj);
+  }, [window.myCustomObj]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-dark">
+      <Header />
+      <div
+        className="mySection d-flex justify-content-center gap-2"
+        id="homeSection"
+      >
+        <Home />
+      </div>
+      <div className="mySection" id="experienceSection">
+        <About />
+      </div>
+      <div className="mySection" id="skillsSection">
+        Skills
+      </div>
+      <div className="mySection" id="projectsSection">
+        Projects
+      </div>
     </div>
   );
 }
